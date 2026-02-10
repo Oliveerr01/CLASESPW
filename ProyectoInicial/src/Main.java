575e5943c488b1e68d50159061cf830425f0e241
Oliveerr01 <olivergonzalesp.01@gmail.com>
2026-02-10 19:55:23 +0100

    update

--- ProyectoInicial/src/Main.java
+++ ProyectoInicial/src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+
+    //esto es un comentario
+    /*
+    este comentario se puede dividir
+    en varias
+    lineas de código
+     */
+
+    //TODO me falta esto
+    public static void main(String[] args){
+        System.out.println("Este es mi primer programa en Java");
+        System.out.println("Este es la segunda linea del programa");
+
+
+    }
+    public void nombreMeotodo (){
+
+    }
 }
