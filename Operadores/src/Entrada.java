8f0256e26749179c737b7cb4d3d2457f119203a0
Oliveerr01 <olivergonzalesp.01@gmail.com>
2026-02-17 19:04:19 +0100

    update

---
+++ Operadores/src/Entrada.java
@@ -0,0 +1,9 @@
+public class Entrada {
+    // esta pestaña del main es donde le dare al play para hacer run el programa.
+    public static void main (String [] args) {
+        Operadores operadoresVariable = new Operadores();
+        //operadoresVariable.operadoresAritmeticos (); //ponemos a el aritmetico o el de comparación para que se ejecute
+        //operadoresVariable.operadoresComparacion ();
+        operadoresVariable.operadoresLogicos();
+    }
+}
---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,72 @@
+public class Operadores {
+    public void operadoresAritmeticos() {
+        System.out.println("Explicacion de los operadores aritméticos");
+        //unarios solo necesito un operando
+        int operandoUno = 5;
+        operandoUno++; //operandoUno = operandoUno+1
+        operandoUno++;
+        operandoUno++;
+        operandoUno++;
+        System.out.println("Después de incrementar:"+operandoUno);
+        operandoUno--; //operandoUno = operandoUno-1
+        operandoUno--;
+        operandoUno--;
+        System.out.println("Después de decrementar:"+operandoUno);
+
+        //binarios
+        int operandoDos = 5;
+        int suma = operandoUno+operandoDos;
+        System.out.println("El resultado de la suma es:" + suma);
+        int resta = operandoUno - operandoDos;
+        System.out.println("El resultado de la resta es:" + resta);
+        int multiplicacion = operandoUno * operandoDos;
+        System.out.println("El resultado de la multiplicación es:" + multiplicacion);
+        double division = (double) operandoUno / operandoDos; //el double es para cambiar el operandoUno a double para que de decimal el rs
+        System.out.println("El resultado de la divisón es:" + division);
+        System.out.printf("El resultado de la división es %4f", division);
+        int modulo = operandoUno % operandoDos; //el resto de al división
+        System.out.println(modulo);
+    }
+
+    public void operadoresAsignacion() {
+        int operandoUno = 6;
+        operandoUno += 6; //operandoUno = operandoUno + 6; // el resultado de ambas da 12
+        operandoUno -= 3; // 9
+        operandoUno *= 2; //18
+        operandoUno /= 2; // 9
+        operandoUno %= 3; // 0
+    }
+
+    public void operadoresComparacion() {
+        int numeroUno = 1;
+        int numeroDos = 4;
+
+        boolean resultado = numeroUno > numeroDos; //false
+        System.out.println("elresultado de > es:" + resultado);
+        resultado = numeroUno < numeroDos; // true
+        System.out.println("elresultado de < es:" + resultado);
+        resultado = numeroUno == numeroDos; //false
+        System.out.println("elresultado de == es:" + resultado);
+        resultado = numeroUno != numeroDos; // true
+        System.out.println("elresultado de != es:" + resultado);
+
+    }
+
+    public void operadoresLogicos() {
+        int numeroUno = 4;
+        int numeroDos = 10;
+        int numeroTres = 17;
+
+        boolean resultadoLogicoAND = numeroUno > 1 && numeroDos < 11 && numeroDos != numeroTres && numeroTres % 2 != 0;
+//   true        &&     true     &&        true           &&         True  --> true
+        System.out.println("El resultado logico del AND es " + resultadoLogicoAND);
+
+        boolean resultadoLogicoOR = numeroUno != 10 || numeroDos > 10 || numeroTres < numeroDos || numeroDos % 2 != 0;
+//     true  ||      false   ||        false         ||         false   --> true
+        System.out.println("El resultado logico de OR es: " + resultadoLogicoOR);
+
+        boolean resultadoCon = numeroUno < 10 || numeroTres > 0 || numeroDos != 9 & numeroUno > 0;
+// true     ||    true      ||    true      &    true --> true
+        System.out.println("El resultado logico de OR y AND es: " + resultadoCon);
+    }
+}
