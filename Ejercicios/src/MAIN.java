cd2895fe3872ba27b4eee39f24f71af585dddbb5
Oliveerr01 <olivergonzalesp.01@gmail.com>
2026-03-23 20:37:57 +0100

    update

--- Ejercicios/src/MAIN.java
+++ Ejercicios/src/MAIN.java
@@ -2,38 +2,20 @@ import java.util.Scanner;
 
 public class MAIN {
     public static void main(String[] args) {
-        Scanner lectorTeclado = new Scanner(System.in);
-        System.out.println("----EJERCICIOS----");
-        System.out.println( "1. Flexiones");
-        System.out.println("2. Abdominales");
-        System.out.println("3. Sentadillas");
-
-        System.out.println("Elige un ejercicio (1-3): ");
-        int ejercicio = lectorTeclado.nextInt();
-        System.out.println("Cuantas repeticiones?");
-        int repes = lectorTeclado.nextInt();
-
-        String nejercicio = "";
-        switch (ejercicio){
-            case 1:
-                nejercicio="Flexiones";
-                break;
-            case 2:
-                nejercicio="Abdominales";
-                break;
-            case 3:
-                nejercicio="Sentadillas";
-                break;
-            default:
-                System.out.println("Elige un ejercicio válido");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cuantos estudiantes hay?");
+        int numestudiantes = scanner.nextInt();
+        int estudiante=1;
+        int nota;
+        while (estudiante<=numestudiantes){
+            System.out.println("Introduce nota estudiante "+estudiante+" (0-10)");
+            nota = scanner.nextInt();
+            estudiante++;
         }
-        System.out.println("Has elegido "+nejercicio);
-        for (int i=1; i<=repes; i++){
-            System.out.println("Repetición "+i+" completada");
+        System.out.println("---REPORTE CALIFICACIONES---");
+        for (int i = 1; i <=numestudiantes; i++) {
+            System.out.println("Estudiante "+i+nota+" puntos");
+            
         }
-        System.out.println("Ejercicio completado. Has hecho "+repes+" "+nejercicio);
-        lectorTeclado.close();
     }
-
-
 }
